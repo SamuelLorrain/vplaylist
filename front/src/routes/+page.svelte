39c4d0551e00af50f7e2f7c3e1f3494c9a9635c0
SamuelLorrain <samuel.lorrain@gmail.com>
2023-05-14 01:05:04 +0200

    web can refetch playlist

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
+    import { invalidateAll } from '$app/navigation';
+    import { onMount } from 'svelte';
     import type { PageData } from './$types';
     export let data : PageData;
+    let videoElement;
     let number: int = 0;
+
     const plus = () => (number >= data.length) ? '' : number += 1;
     const minus = () => number <= 0 ? '' : number -= 1;
+    const fetchPlaylist = () => {
+        invalidateAll();
+    }
+
     $: current = data.playlist[number];
     $: videoSrc = `http://localhost:8000/video/${current.uuid}`;
+
+    onMount(() => {
+        videoElement.addEventListener("ended", () => {
+            number += 1;
+        });
+    })
 </script>
 
 <section>
     <h1>Player</h1>
-    <video controls src={videoSrc} width="500" autoplay>
+    <video bind:this={videoElement} controls src={videoSrc} width="500" autoplay>
     </video>
     <button on:click={minus}>-</button>
     <button on:click={plus}>+</button>
+    <button on:click={fetchPlaylist}>Another playlist</button>
     {#each data.playlist as video (video.uuid)}
     <div>
         {#if video.uuid === current.uuid}
