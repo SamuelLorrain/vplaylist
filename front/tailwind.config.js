f369d4f983ae5f53c37b7c80d0f29994af07365a
SamuelLorrain <samuel.lorrain@gmail.com>
2023-06-23 21:58:28 +0200

    update shadcnui

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
 	],
   theme: {
     container: {
       center: true,
+      padding: "2rem",
       screens: {
         "2xl": "1400px",
       },
